c5d00da69c6363df10128d6714cda054a2abebfe
ppp103 <74097281+ppp103@users.noreply.github.com>
2022-03-01 22:32:40 +0700

    khung game

---
+++ src/com/model/PongGame.java
@@ -0,0 +1,12 @@
+package com.model;
+
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class PongGame {
+    public static void main(String[] args) {
+
+    }
+}
