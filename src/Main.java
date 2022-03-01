1493fb7cd664ef3f9a4f566584236226d5ccd3b8
ppp103 <74097281+ppp103@users.noreply.github.com>
2022-03-01 21:59:11 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,5 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hello again");
         System.out.println("Here í it");
-        System.out.println("tminh đỉnh vcl");
     }
 }
